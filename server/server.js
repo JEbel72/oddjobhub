dfb3b6498cbd5fa0a7e16559ccac8f768acfd33b
JEbel72 <jacobpebel@gmail.com>
2024-03-13 17:13:31 -0700

    updated server #2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors({origin:'http://localhost:5173/', credentials:true}))
+app.use(cors({origin:'http://localhost:5173', credentials:true}))
 app.use(express.json(), express.urlencoded({ extended: true }))
 
 const cookieParser = require('cookie-parser')
